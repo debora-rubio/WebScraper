284a0116c113b1b486620d94d41cbf053a3d6ad6
debora-rubio <debora.rubio@fatec.sp.gov.br>
2026-04-14 10:45:02 -0300

    inserção do jornal Estadão para o raspador fazer as buscas.

--- raspador.py
+++ raspador.py
@@ -1,21 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL BBC.
-url = "https://www.bbc.com/portuguese"
+def raspador_bbc():
+    url = "https://www.bbc.com/portuguese"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    titulos = soup.find_all("h3")
+    print("\nManchetes da BBC:\n")
+    for i, titulo in enumerate(titulos[:5], start=1):
+        print(f"{i}. {titulo.get_text(strip=True)}")
 
-# Faz a requisição HTTP
-response = requests.get(url)
+def raspador_estadao():
+    url = "https://www.estadao.com.br/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    titulos = soup.find_all("h3")
+    print("\nManchetes do Estadão:\n")
+    for i, titulo in enumerate(titulos[:5], start=1):
+        print(f"{i}. {titulo.get_text(strip=True)}")
 
-# Cria o objeto BeautifulSoup
-soup = BeautifulSoup(response.text, "html.parser")
+# Programa principal
+if __name__ == "__main__":
+    raspador_bbc()
+    raspador_estadao()
 
-# Encontra os títulos das notícias (nesse caso, tags <h3>)
-titulos = soup.find_all("h3")
-
-print("Manchetes encontradas:\n")
-for i, titulo in enumerate(titulos[:5], start=1):  # 5 primeiros titulos h3 do jornal
-    print(f"{i}. {titulo.get_text(strip=True)}")
 
 
 
