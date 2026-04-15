b6faa6950505e1641779baaae0fcbd8374841bca
debora-rubio <debora.rubio@fatec.sp.gov.br>
2026-04-15 10:59:46 -0300

    inserindo comentarios explicativos nas linhas do codigo.

--- raspador.py
+++ raspador.py
@@ -9,21 +9,21 @@ class Raspador:
     def coletar_manchetes(self, limite=5):         # método para coletar as manchetes, limite de 5 manchetes por jornal.
         print(f"\nManchetes do {self.nome}:\n")    
         try:                                       # avisa se der erro ao acessar o site do jornal.
-            response = requests.get(self.url)      # 
-            soup = BeautifulSoup(response.text, "html.parser")
-            titulos = soup.find_all("h3")
-            for i, titulo in enumerate(titulos[:limite], start=1):
-                print(f"{i}. {titulo.get_text(strip=True)}")
+            response = requests.get(self.url)      # baixa o html do site do jornal.
+            soup = BeautifulSoup(response.text, "html.parser")  # interpreta o html usando o beautifulsoup.
+            titulos = soup.find_all("h3")          # h3 é a tag dos titulos das manchetes.
+            for i, titulo in enumerate(titulos[:limite], start=1):    # exibe as manchetes a partir da primeira.
+                print(f"{i}. {titulo.get_text(strip=True)}")    # removendo espaços em branco (get_text(strip=True)).
         except Exception as e:
-            print(f"Erro ao acessar {self.nome}: {e}")
+            print(f"Erro ao acessar {self.nome}: {e}")   # retorna erro se o site estiver fora do ar, ou alteração na estrutura do site.
 
-# Programa principal
+                                                     # Programa principal
 if __name__ == "__main__":
-    # Criando objetos para cada jornal
+                                                     # Criando objetos para cada jornal
     bbc = Raspador("BBC", "https://www.bbc.com/portuguese")
     estadao = Raspador("Estadão", "https://www.estadao.com.br/")
 
-    # Chamando o método para coletar manchetes
+                                                     # Chamando o método para coletar manchetes
     bbc.coletar_manchetes()
     estadao.coletar_manchetes()
 
